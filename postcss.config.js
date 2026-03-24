5b23d0ca6797b468ef702e4c6f89c3834ab1ce56
fredbello12-devs <fredbello12@gmail.com>
2026-03-24 23:56:33 +0000

    résolution du conflit dans la configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,6 @@
 module.exports = {
   plugins: {
-<<<<<<< HEAD
-    "@tailwindcss/postcss": {},
-    autoprefixer: {},
-  },
-};
-=======
     tailwindcss: {},
     autoprefixer: {},
   },
-}
->>>>>>> c5415d056d589c49d66840a0c2c584b48b6e522b
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        display: ["'Bebas Neue'", "cursive"],
-        heading: ["'Barlow Condensed'", "sans-serif"],
-        body: ["'Inter'", "sans-serif"],
-      },
-    },
-  },
-  plugins: [],
-};
-=======
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
->>>>>>> c5415d056d589c49d66840a0c2c584b48b6e522b
+};
